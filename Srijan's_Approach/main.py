22ac6c7a710bf82956063f55a1727a1e067b0e10
Srijan Singh <srijanssj5goku@gmail.com>
2022-02-25 02:32:23 +0530

    result dict

--- Srijan's_Approach/main.py
+++ Srijan's_Approach/main.py
@@ -6,6 +6,9 @@ class Solution:
         self.input_list = list()
         self.contributors = dict()
         self.projects = dict()
+        self.result = dict()
+        self.project_completed = list()
+        
 
     # Prepare Input
 
@@ -26,7 +29,7 @@ class Solution:
             print(i)
 
     # Shreshta's Parser
-    def fileParser(self, path):
+    def fileParser(self,path):
         with open(path, 'r') as f:
             lines = f.readlines()
             # print(lines)
@@ -50,7 +53,7 @@ class Solution:
 
                     for j in range(i, num_skills+i):
                         line = lines[j].split()
-                        contribs[name].append((line[0], int(line[1])))
+                        contribs[name].append([line[0], int(line[1])])
                     i += num_skills
 
                 else:
@@ -63,8 +66,7 @@ class Solution:
                         i += 1
                     else:
                         projs[proj_name].append((line[0], int(line[1])))
-                        i += 1
-            # print(contribs, projs)
+                        i += 1        # print(contribs, projs)
             self.contributors = contribs
             self.projects = projs
             return 
@@ -72,7 +74,10 @@ class Solution:
     def find_candidate(self):
         for i in s.projects.keys():
         #print(i)
-            s.eligible_candidate(i)
+            if i in self.project_completed:
+                continue
+            else:
+                s.eligible_candidate(i)
 
     def eligible_candidate(self, project):
         num_skills = int(self.projects[project][3])
@@ -87,9 +92,24 @@ class Solution:
                     
                     if (k[0] == skill[0]):
                         if(int(k[1]) >= int(skill[1])):
+                            
+                            k[1]+=1
+
+                            """
+                            if self.result[project] == None:
+                                self.result[project] = [j]
+                            else:
+                                self.result[project].append(j)
+                                """
+                            
                             print("match")
                             print(j,k)
                             print(project)
+                            self.project_completed.append(project)
+
+                            self.find_candidate()
+                            
+
                             
              
 
