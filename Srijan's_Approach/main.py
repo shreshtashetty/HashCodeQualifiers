7c27f8acd8b5beb5a2adeb03fc08594794ca8ea5
Srijan Singh <srijanssj5goku@gmail.com>
2022-02-24 23:59:22 +0530

    Test Cases

--- Srijan's_Approach/main.py
+++ Srijan's_Approach/main.py
@@ -1,6 +1,6 @@
 def read_file(file_path : str) -> str:
     my_file = open(file_path, "r")
-    return my_file
+    return my_file.read()
 
 if __name__ == "__main__":
     print(read_file("a_an_example.in.txt"))
